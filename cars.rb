96e665e18425cb5738a5f619b2e7c79c0b77a713
AlliZad <allison@zadrozny.com>
2014-06-18 10:15:14 -0500

    Add class variable hash to keep track of car instances' colors

--- cars.rb
+++ cars.rb
@@ -8,14 +8,28 @@ class Car
 		@@total_car_count
 	end
 
+	#class variable hash that keeps track of number of vehicles associated with each color.
+	@@cars_per_color = {}
+	attr_reader :cars_per_color
+
 	def to_s()
 		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize()
+	def initialize(color)
+		@color = color
 		@fuel = 10
 		@distance = 0
 		@@total_car_count += 1
+
+		#add car color to the cars_per_color hash, 
+		#where key = color.to_sym, and value = count of car instances with that color
+		if @@cars_per_color.has_key?(@color) == true
+			@@cars_per_color[@color] += 1
+		else
+			@@cars_per_color[@color] = 1
+		end
+
 	end
 
 	def drive(miles)
@@ -36,8 +50,8 @@ class Car
 	end
 end
 
-car_a = Car.new()
-car_b = Car.new()
+car_a = Car.new("white")
+car_b = Car.new("red")
 puts car_a
 puts car_b
 car_a.drive(10)
@@ -49,7 +63,7 @@ puts car_a
 puts car_b
 
 puts Car.total_car_count
-c1 = Car.new
+c1 = Car.new("black")
 puts Car.total_car_count
-c2 = Car.new
+c2 = Car.new("red")
 puts Car.total_car_count
